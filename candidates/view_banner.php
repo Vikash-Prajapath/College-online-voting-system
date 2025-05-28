6b8663a8005fb43f29ea1e468d9ff416f7700537
Vikash Prajapath <156921304+Vikash-Prajapath@users.noreply.github.com>
2025-05-28 19:22:01 +0530

    Add files via upload

---
+++ candidates/view_banner.php
@@ -0,0 +1,18 @@
+
+<div class="side-bar" 
+            style="background-color:rgba(0, 100, 0, 1); font-size:25px; text-align:center; margin-top:-10px; padding-top:20px;">
+
+                <a style="color:white; font-weight:bold; font-size:30px;">College Online Voting System</a>
+                </a>
+
+<nav class="nav-menue">
+            <ul>
+                <li>
+                    <a href="../index.php">Home</a>
+                </li>
+                <li><a href="../view.php">Candidates</a></li>
+                <li><a href="../register/index.php">Register</a></li>
+            </ul>
+</nav>
+					
+</div>
